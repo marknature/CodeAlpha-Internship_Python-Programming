01a287ccbbf565bf709a1096151fa871593a77e4
Mark Chindudzi <chindudzim@africau.edu>
2025-06-13 22:13:57 +0200

    function

---
+++ Task_2.py
@@ -0,0 +1,13 @@
+def calculate_portfolio_value():
+    """
+    Calculates the total investment value of a stock portfolio based on user input and hardcoded stock prices.
+    """
+
+    stock_prices = {
+        "AAPL": 180,
+        "TSLA": 250,
+        "GOOG": 2500,
+        "AMZN": 150
+    }
+
+    portfolio = {}
